816afc8503055a3a9501bc08235de74d55b0a648
Mark Cesario <mark.cesario@gmail.com>
2020-04-23 15:32:42 -0600

    done with 48 Extending Base Templates

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display= ('title', 'description', )
+    list_filter = ('title',)
+
+admin.site.site_header = 'Portfolio Project Admin'
+admin.site.register(Project, ProjectAdmin)
