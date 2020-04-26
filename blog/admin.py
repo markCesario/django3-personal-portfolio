74fae847e238bfc1dccdc7ba2ea967a6e646108b
Mark Cesario <mce@Marks-MacBook-Air.local>
2020-04-25 20:11:05 -0600

    staring section 6

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . models import Blog
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display= ('title', 'description', )
+    list_display= ('title', )
     list_filter = ('date',)
 
 admin.site.register(Blog, BlogAdmin)
